e5f89b72240d715a58caab9b3eac67c49e7999d5
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-05-24 16:46:45 +0200

    added collect last sales values func

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
+#def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
     """
@@ -51,6 +51,13 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("sales worksheet updated successfully.\n")
+
+
+def update_worksheet(data, worksheet):
+    print(f"Updating {worksheet} worksheet ...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully.\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -66,16 +73,39 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+#def update_surplus_worksheet(new_surplus_data):
+    """
+    Update surplus worksheet, add new row with the list data calculated
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(new_surplus_data)
+    print("Surplus worksheet updated successfully.\n")
+
+
+def get_last_entries_sales():
+    sales = SHEET.worksheet('sales')
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
+
 def main():
     """
     Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print ("Welcome to Sandwich Shop data automation")
-main()
+#main()
+
+sales_columns = get_last_entries_sales()
